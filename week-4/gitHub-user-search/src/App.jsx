3fcce6485793f7a7789a3d38ae3e0e9c8e31c558
Jim Kennedy <kennedyomata@gmail.com>
2025-05-16 12:26:12 +0300

    initial files including axios

---
+++ week-4/gitHub-user-search/src/App.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CreatePost from "./Createpost";
+
+const App = () => {
+  return <GetQuote />;
+};
+
+export default App;
