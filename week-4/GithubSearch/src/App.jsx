aabea0b95cad610f78dbc5f1f17619f0260937c3
Jim Kennedy <kennedyomata@gmail.com>
2025-05-26 15:17:20 +0300

    modifiied css

--- week-4/GithubSearch/src/App.jsx
+++ week-4/GithubSearch/src/App.jsx
@@ -75,11 +75,11 @@ function App() {
     <div
       className={`${
         theme === "dark" ? "dark" : ""
-      } min-h-screen min-w-screen flex flex-col bg-gray-300 dark:bg-gray-800/100`}
+      } min-h-screen w-screen flex flex-col bg-gray-300 dark:bg-gray-800/100`}
     >
       <div
         className="
-         max-w-2xl flex-1 mx-auto mb-5  px-4 sm:px-6 lg:px-8 "
+          flex-1 mx-auto px-4 sm:px-6 lg:px-8 "
       >
         <div className="flex flex-row justify-between p-3 text-xl sm:text-2xl text-blue-950 dark:text-blue-400">
           <h1 className="font-bold ">devfinder Clone</h1>
@@ -123,11 +123,11 @@ function App() {
         {/* Github Profile Card */}
 
         {isloading ? (
-          <div className="flex justify-center items-center ">
+          <div className="flex justify-center items-center min-h-[200px]">
             <h1 className="text-emerald-600 text-2xl font-bold">Loading...</h1>
           </div>
         ) : error ? (
-          <div className="flex justify-center items-center">
+          <div className="flex justify-center items-center min-h-[200px">
             <h1 className="text-2xl text-red-600 font-bold">{error}</h1>
           </div>
         ) : (
