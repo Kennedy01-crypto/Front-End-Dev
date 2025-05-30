5db008eb3dfa2a61c4b32545135c00eed96a6d5f
Jim Kennedy <kennedyomata@gmail.com>
2025-05-30 23:46:16 +0300

    added week 7

---
+++ Week-7/ShoppingCart/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState, useContext } from "react";
+import { Counter } from "./features/counter/Counter.jsx";
+
+export default function App() {
+  return (
+    <>
+      <Counter />
+    </>
+  );
+}
