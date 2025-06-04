1ea4edbb0e3f46f6026c052bf661d4bff393232c
Jim Kennedy <kennedyomata@gmail.com>
2025-06-04 22:13:12 +0300

    added context provider

--- Week-7/ShoppingCart/src/App.jsx
+++ Week-7/ShoppingCart/src/App.jsx
@@ -7,42 +7,45 @@ import {
 } from "react-router-dom";
 import { Shop } from "./components/Shop.jsx";
 import { Cart } from "./components/Cart.jsx";
+import { ItemsProvider } from "./context/ItemsContext";
 
 export default function App() {
   return (
-    <div>
-      <Router>
-        <nav className="flex space-x-4 p-4 bg-gray-800 text-white">
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              `font-bold px-3 py-2 rounded-md ${
-                isActive
-                  ? "bg-blue-600 text-white"
-                  : "text-gray-300 hover:bg-gray-700"
-              }`
-            }
-          >
-            Shop
-          </NavLink>
-          <NavLink
-            to="/cart"
-            className={({ isActive }) =>
-              `font-bold px-3 py-2 rounded-md ${
-                isActive
-                  ? "bg-blue-600 text-white"
-                  : "text-gray-300 hover:bg-gray-700"
-              }`
-            }
-          >
-            Cart
-          </NavLink>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Shop />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-      </Router>
-    </div>
+    <ItemsProvider>
+      <div>
+        <Router>
+          <nav className="flex  space-x-4 p-4 bg-gray-800 text-white">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                `font-bold px-3 py-2 rounded-md ${
+                  isActive
+                    ? "bg-blue-600 text-white"
+                    : "text-gray-300 hover:bg-gray-700"
+                }`
+              }
+            >
+              Shop
+            </NavLink>
+            <NavLink
+              to="/cart"
+              className={({ isActive }) =>
+                `font-bold px-3 py-2 rounded-md ${
+                  isActive
+                    ? "bg-blue-600 text-white"
+                    : "text-gray-300 hover:bg-gray-700"
+                }`
+              }
+            >
+              Cart
+            </NavLink>
+          </nav>
+          <Routes>
+            <Route path="/" element={<Shop />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Router>
+      </div>
+    </ItemsProvider>
   );
 }
