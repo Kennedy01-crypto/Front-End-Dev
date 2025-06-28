a9a769c26f7e317b34466a3d7ecdf068df986cb2
Jim Kennedy <kennedyomata@gmail.com>
2025-06-28 21:57:54 +0300

    merged user details into an object

--- week-4/GithubSearch/src/hooks/useGithubUser.js
+++ week-4/GithubSearch/src/hooks/useGithubUser.js
@@ -2,16 +2,7 @@ import { useState } from "react";
 
 function useGithubUser(initialUsername = "octocat") {
   const [username, setUsername] = useState(initialUsername);
-  const [devusername, setDevUsername] = useState("");
-  const [user, setUser] = useState("");
-  const [avatar, setAvatar] = useState("");
-  const [location, setLocation] = useState("");
-  const [publicRepos, setPublicRepos] = useState("");
-  const [followers, setFollowers] = useState("");
-  const [following, setFollowing] = useState("");
-  const [bio, setBio] = useState("");
-  const [url, setUrl] = useState("");
-  const [joinDate, setJoinDate] = useState("");
+  const [user, setUser] = useState(null);
   const [isloading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
@@ -27,22 +18,22 @@ function useGithubUser(initialUsername = "octocat") {
         throw new Error("User not found!");
       }
       const data = await response.json();
-      setDevUsername(data.login);
-      setUser(data.name);
-      setAvatar(data.avatar_url);
-      setLocation(data.location);
-      setPublicRepos(data.public_repos);
-      setFollowers(data.followers);
-      setFollowing(data.following);
-      setBio(data.bio);
-      setJoinDate(
-        new Date(data.created_at).toLocaleDateString("en-US", {
+      setUser({
+        devusername: data.login,
+        name: data.name,
+        avatar: data.avatar_url,
+        location: data.location,
+        public_repos: data.public_repos,
+        followers: data.followers,
+        following: data.following,
+        bio: data.bio,
+        url: data.html_url,
+        joinDate: new Date(data.created_at).toLocaleDateString("en-US", {
           year: "numeric",
           month: "long",
           day: "numeric",
-        })
-      );
-      setUrl(data.html_url);
+        }),
+      });
     } catch (error) {
       setError(error.message);
     } finally {
@@ -53,16 +44,7 @@ function useGithubUser(initialUsername = "octocat") {
   return {
     username,
     setUsername,
-    devusername,
     user,
-    avatar,
-    location,
-    publicRepos,
-    followers,
-    following,
-    bio,
-    url,
-    joinDate,
     isloading,
     error,
     fetchUser,
